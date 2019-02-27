0c9f01a64e25fc960bce8f7a5e5477bcaaca84e0
Ken5998 <kenan@kasumovic.ch>
2019-02-27 19:57:48 +0100

    first commit

--- src/Hellotutti.java
+++ src/Hellotutti.java
@@ -1,7 +1,7 @@
 public class Hellotutti {
     public static void main(String[] args) {
-        System.out.println("Hello Kenan 6.0!");
-        System.out.println("hello my friend");
+        System.out.println("Hello Kenan 5.0!");
+        System.out.println("hello my big friend");
     }
 }
 
