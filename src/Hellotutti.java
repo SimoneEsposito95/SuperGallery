e7de86ad958c3d09eb82e33507d199fa659dbfde
SimoneEsposito95 <simone.esposito@student.supsi.ch>
2019-02-27 19:48:42 +0100

    5.0

--- src/Hellotutti.java
+++ src/Hellotutti.java
@@ -1,6 +1,6 @@
 public class Hellotutti {
     public static void main(String[] args) {
-        System.out.println("Hello Kenan 3.0!");
+        System.out.println("Hello Kenan 5.0!");
         System.out.println("hello my friend");
     }
 }
