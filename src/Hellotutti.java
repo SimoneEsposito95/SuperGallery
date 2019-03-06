217a069a95920aaebac5d94a29be8fc2267386ef
SimoneEsposito95 <simone.esposito@student.supsi.ch>
2019-03-06 17:46:03 +0100

    Maven project

--- src/Hellotutti.java
+++
@@ -1,6 +0,0 @@
-public class Hellotutti {
-    public static void main(String[] args) {
-        System.out.println("Hello Kenan 5.0!");
-        System.out.println("hello my big friend");
-    }
-}
